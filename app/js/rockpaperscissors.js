7d43369e28c238847acb39ac46e565388b265e44
Yao Yuan <opal.obsidian@gmail.com>
2015-08-02 13:59:56 +0100

    Unit 5 Homework.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -59,12 +59,32 @@ function getWinner(playerMove,computerMove) {
     return winner;
 }
 
-function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+function playTo(x) {
+  console.log('Let\'s play Rock Paper Scissors');
+  var playerWins = 0;
+  var computerWins = 0;
+  // This function should continue to play Rock Paper Scissors until either the
+  // player or the computer has won five times.
+  // After each 'round', display some text in the console indicating who played
+  // what, who won, and what the current scoreboard looks like.
+  // For example,
+  //  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+  //  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+  /* YOUR CODE HERE */
+  while ( (playerWins < x) && (computerWins < x) ) {
+      var playerMove = getPlayerMove();
+      var computerMove = getComputerMove();
+      var winner = getWinner (playerMove, computerMove);
+      if (winner === 'player') {
+          playerWins++;
+      }
+      else if (winner === 'computer') {
+          computerWins++;
+      }
+      console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+      console.log('The score is currently Player: ' + playerWins + ' , Computer: ' + computerWins + '\n');
+  }
+  return [playerWins, computerWins];
 }
+
 
